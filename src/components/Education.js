f774d7e655336659cfb12e51e1dca2e452c82635
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-08-30 13:19:23 +0500

    Update Experience comp for form and display

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,12 +1,67 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export default class Education extends React.Component {
   constructor() {
     super();
-    this.state = { education: [] };
+    this.state = {
+      education: [],
+      current: {
+        id: uuidv4(),
+        name: "",
+        title: "",
+        from: "",
+        to: "",
+        state: "form",
+      },
+    };
   }
 
-  Experience = ({ name, title, from, to }) => {
+  alterState = (e) => {
+    console.log(e.target);
+  };
+
+  Experience = ({ id, name, title, from, to, state }) => {
+    if (state === "form") {
+      return (
+        <form id={id} onSubmit={this.alterState}>
+          <label>
+            Institution Name:
+            <input
+              name="schoolName"
+              placeholder="Cedar College"
+              type="text"
+              defaultValue={name}
+              required
+            />
+          </label>
+          <label>
+            Education:
+            <input
+              name="education"
+              placeholder="A levels"
+              type="text"
+              defaultValue={title}
+              required
+            />
+          </label>
+          <label>
+            From:
+            <input name="from" type="date" defaultValue={from} required />
+          </label>
+          <label>
+            To:
+            <input name="to" type="date" defaultValue={to} required />
+          </label>
+          <button type="submit" name="send" value="save">
+            Save
+          </button>
+          <button type="submit" name="send" value="delete">
+            Delete
+          </button>
+        </form>
+      );
+    }
     return (
       <div>
         <div>Institution Name: {name}</div>
@@ -18,12 +73,33 @@ export default class Education extends React.Component {
     );
   };
 
+  addEducation = () => {
+    this.setState({
+      education: [...this.state.education, this.state.current],
+      current: {
+        id: uuidv4(),
+        name: "",
+        title: "",
+        from: "",
+        to: "",
+        state: "form",
+      },
+    });
+  };
+
   render() {
     return (
       <div>
         <div class="main-heading">Education</div>
-        {this.state.education.map(({ name, title, from, to }) => {
-          <this.Experience name={name} title={title} from={from} to={to} />;
+        {this.state.education.map(({ id, name, title, from, to, state }) => {
+          <this.Experience
+            id={id}
+            name={name}
+            title={title}
+            from={from}
+            to={to}
+            state={state}
+          />;
         })}
         <button onClick={addEducation}>Add</button>
       </div>
