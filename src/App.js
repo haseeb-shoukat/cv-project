bcd728fff659e104681d0cb9770199a6e893db2d
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-08-31 06:35:23 +0500

    Add main div for styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import General from "./components/General";
 import Education from "./components/Education";
+import Work from "./components/Work";
 
 function App() {
   return (
-    <div>
+    <div className="main">
       <General />
       <Education />
+      <Work />
     </div>
   );
 }
